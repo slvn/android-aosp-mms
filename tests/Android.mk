aff6e54a293214e310d041b92edaffd7f26e7857
slvn <sylvain.galand@gmail.com>
2013-11-12 21:03:45 +0100

    Change already defined module name (tests)

--- tests/Android.mk
+++ tests/Android.mk
@@ -27,7 +27,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 # running the tests using an instrumentation targeting Eamil, we
 # automatically get all of its classes loaded into our environment.
 
-LOCAL_PACKAGE_NAME := MmsTests
+LOCAL_PACKAGE_NAME := MmsTests2
 
 LOCAL_INSTRUMENTATION_FOR := Mms
 
